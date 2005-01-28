078af56bd3033e3b96adc50ec68c2186fa537206
peachey <>
2005-01-28 20:40:58 +0000

    Use pulsarDb classes instead of original pulsePhase::TimingModel.

--- src/test/test_pulsePhase.cxx
+++ src/test/test_pulsePhase.cxx
@@ -2,15 +2,19 @@
 #include <cstdlib>
 #include <iostream>
 #include <limits>
+#include <memory>
 
-#include "pulsePhase/TimingModel.h"
+#include "pulsarDb/GlastTime.h"
+#include "pulsarDb/PulsarEph.h"
+#include "pulsarDb/TimingModel.h"
 
 #include "st_facilities/Env.h"
 
+#include "tip/Header.h"
 #include "tip/IFileSvc.h"
 #include "tip/Table.h"
 
-using namespace pulsePhase;
+using namespace pulsarDb;
 
 int main() {
   int status = 0;
@@ -22,83 +26,30 @@ int main() {
   std::string data_dir = st_facilities::Env::getDataDir("pulsePhase");
 
   // Simple test of timing model computation.
-  TimingModel model(123.456789, .11, 1.125e-2, -2.25e-4, 6.75e-6);
+  TimingModel model;
 
-  double phase = model.calcPhase(223.456789);
-
-  double epsilon = 1.e-8;
-
-  // Result determined independently.
-  if (fabs(phase/.235 - 1.) > epsilon) {
-    status = 1;
-    std::cerr << "ERROR: calc_phase produced phase == " << phase << " not .235" << std::endl;
-  }
-
-  // Test FrequencyCoeff class:
-  TimingModel model2(123.456789, 0.11, TimingModel::FrequencyCoeff(1.125e-2, -2.25e-4, 6.75e-6));
-  double phase2 = model2.calcPhase(223.456789);
-  if (fabs(phase2/phase - 1.) > epsilon) {
-    status = 1;
-    std::cerr << "ERROR: calc_phase using FrequencyCoeff produced phase == " << phase2 << " not " << phase << std::endl;
-  }
-
-  // Test PeriodCoeff class:
-  // This is a set of values known to be the inverses of the frequency coefficients above.
-  TimingModel::PeriodCoeff pc(88.8888888888888888888889, 1.777777777777777777777778, 0.0177777777777777777777778);
-
-  TimingModel model3(123.456789, 0.11, pc);
-  double phase3 = model3.calcPhase(223.456789);
-  if (fabs(phase3/phase - 1.) > epsilon) {
-    status = 1;
-    std::cerr << "ERROR: calc_phase using PeriodCoeff produced phase == " << phase3 << " not " << phase2 << std::endl;
-  }
- 
   // Next test: read event file.
-  tip::Table * events = tip::IFileSvc::instance().editTable(st_facilities::Env::appendFileName(data_dir, "D1.fits"), "EVENTS");
+  std::auto_ptr<tip::Table> events(tip::IFileSvc::instance().editTable(st_facilities::Env::appendFileName(data_dir, "D1.fits"), "EVENTS"));
  
+  double valid_since = 0.;
+  double valid_until = 0.;
+
+  const tip::Header & header(events->getHeader());
+  header["TSTART"].get(valid_since);
+  header["TSTOP"].get(valid_until);
+
+  GlastTdbTime abs_valid_since(valid_since);
+  FrequencyEph eph(abs_valid_since, GlastTdbTime(valid_until), GlastTdbTime(123.456789), .11, 1.125e-2, -2.25e-4, 6.75e-6);
+
   // Iterate over events.
   for (tip::Table::Iterator itor = events->begin(); itor != events->end(); ++itor) {
     tip::Table::Record & rec = *itor;
     // Calculate phase.
-    double phase = model.calcPhase(rec["TIME"].get());
+    double phase = model.calcPhase(eph, GlastTdbTime(rec["TIME"].get()));
 
     // Write phase into output column.
     rec["PULSE_PHASE"].set(phase);
   }
 
-  delete events;
-
-  // Test calcPdotCorr method.
-  // Change coefficients to produce a visible effect.
-  TimingModel model4(123.4567891234567, .11, 1.125e-2, -2.25e-4, 13.5e-6);
-  double evt_time = 223.4567891234567;
-  double pdot_t = model4.calcPdotCorr(evt_time);
-  double correct_t = 323.4567891234567;
-  // For this test, time difference between these two values must be << 1.e-6.
-  if (fabs(pdot_t - correct_t) > epsilon) {
-    status = 1;
-    std::cerr << "ERROR: calcPdotCorr produced pdot-corrected time == " << pdot_t << " not " << correct_t << std::endl;
-  }
-  
-  // Test frequency computation.
-  TimingModel::FrequencyCoeff freq = model4.calcFreq(evt_time);
-  double correct_f0 = 5.625e-2;
-  if (fabs(freq.m_term[0] - correct_f0) > epsilon) {
-    status = 1;
-    std::cerr << "ERROR: calcFreq produced f0 == " << freq.m_term[0] << " not " << correct_f0 << std::endl;
-  }
-  
-  double correct_f1 = 11.25e-4;
-  if (fabs(freq.m_term[1] - correct_f1) > epsilon) {
-    status = 1;
-    std::cerr << "ERROR: calcFreq produced f1 == " << freq.m_term[1] << " not " << correct_f1 << std::endl;
-  }
-  
-  double correct_f2 = 13.5e-6;
-  if (fabs(freq.m_term[2] - correct_f2) > epsilon) {
-    status = 1;
-    std::cerr << "ERROR: calcFreq produced f2 == " << freq.m_term[2] << " not " << correct_f2 << std::endl;
-  }
-  
   return status;
 }
