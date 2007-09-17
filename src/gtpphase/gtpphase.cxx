548a77e54b78967e8336287c0edd87e73c1519dd
hirayama <>
2007-09-17 21:45:19 +0000

    Add an EventTimeHandlerFactory object to support GLAST LAT data.

--- src/gtpphase/gtpphase.cxx
+++ src/gtpphase/gtpphase.cxx
@@ -11,6 +11,8 @@
 #include "pulsarDb/TimingModel.h"
 
 #include "timeSystem/AbsoluteTime.h"
+#include "timeSystem/EventTimeHandler.h"
+#include "timeSystem/GlastTimeHandler.h"
 
 #include "st_app/AppParGroup.h"
 #include "st_app/StApp.h"
@@ -142,5 +144,8 @@ void PulsePhaseApp::run() {
   }
 
 }
+
+// List supported mission(s).
+timeSystem::EventTimeHandlerFactory<timeSystem::GlastTimeHandler> glast_handler;
 
 st_app::StAppFactory<PulsePhaseApp> g_factory("gtpphase");
